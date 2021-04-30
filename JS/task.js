6c441799784105bf7cb68dde7ca50ec20a127f2a
galizien <galizien@users.noreply.github.com>
2021-04-30 23:33:54 +0300

    Completed task 1 and 2

--- JS/task.js
+++ JS/task.js
@@ -1,34 +1,17 @@
-let characters = 'abcdefghijklmnopqrstuvwxyz';
-let charactersLength = characters.length;
-
 function generateArray(length) {
-
-     let result = [];
-     for (let i = 0; i < length; i++) {
-          result.push(characters.charAt(Math.floor(Math.random() *
-               charactersLength)));
+     const uniqueSet = new Set();
+     while (uniqueSet.size < length) {
+          const candidate = 97 + Math.floor(Math.random() * 26);
+          uniqueSet.add(candidate);
      }
-     return result;
+     return [...uniqueSet].map((x) => String.fromCharCode(x));
 }
-
 
 let generatedArray = generateArray(5);
-let uniqueArray = [...new Set(generatedArray)];
-
-if (generatedArray.length === 5) {
-     let uniqueArray = [...new Set(generatedArray)];
-     console.log(uniqueArray);
-}
-else {
-     let generatedArray = generateArray(5);
-     console.log(generatedArray);
-
-}
 
 
-
-for (let i = 0, container = document.getElementById("generated-array"); i < uniqueArray.length; i++) {
-     const data = uniqueArray[i];
+for (let i = 0, container = document.getElementById("generated-array"); i < generatedArray.length; i++) {
+     const data = generatedArray[i];
      const new_card = `<div>${data}</div>`;
      container.insertAdjacentHTML("beforeend", new_card);
 }
