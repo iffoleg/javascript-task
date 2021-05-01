8d1581f6c2ee9b82a70f1e0c11ab0cee237ade65
galizien <galizien@users.noreply.github.com>
2021-05-01 18:18:12 +0300

    Changed generateArray() and parsed JSON file

--- JS/task.js
+++ JS/task.js
@@ -1,18 +1,18 @@
-function generateArray(length) {
-     const uniqueSet = new Set();
-     while (uniqueSet.size < length) {
-          const candidate = 97 + Math.floor(Math.random() * 26);
-          uniqueSet.add(candidate);
-     }
-     return [...uniqueSet].map((x) => String.fromCharCode(x));
+const request = new XMLHttpRequest();
+request.open("GET", "list.json", false);
+request.send(null);
+const jsonData = JSON.parse(request.responseText)
+
+let generateArray = (length = 5) => {
+     return [...'abcdefghijklmnopqrstuvwxyz'].sort(() => Math.random() - 0.5).splice(0, length);
 }
 
-let generatedArray = generateArray(5);
-
+let generatedArray = generateArray();
 
 for (let i = 0, container = document.getElementById("generated-array"); i < generatedArray.length; i++) {
      const data = generatedArray[i];
-     const new_card = `<div>${data}</div>`;
-     container.insertAdjacentHTML("beforeend", new_card);
+     const displayedDiv = `<div>${data}</div>`;
+     container.insertAdjacentHTML("beforeend", displayedDiv);
 }
 
+console.log(jsonData);
