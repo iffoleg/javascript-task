4d003984152c7b6ee3a6bf9ba599e0a334135d2f
galizien <galizien@users.noreply.github.com>
2021-05-02 00:11:38 +0300

    Started UI part

--- JS/task.js
+++ JS/task.js
@@ -1,18 +1,30 @@
 const request = new XMLHttpRequest();
 request.open("GET", "list.json", false);
 request.send(null);
-const jsonData = JSON.parse(request.responseText)
+const jsonData = JSON.parse(request.responseText);
 
-let generateArray = (length = 5) => {
+let generateArray = (length = 5) => {  //Function which generates array of random unique letter
      return [...'abcdefghijklmnopqrstuvwxyz'].sort(() => Math.random() - 0.5).splice(0, length);
 }
 
 let generatedArray = generateArray();
 
-for (let i = 0, container = document.getElementById("generated-array"); i < generatedArray.length; i++) {
+for (let i = 0, container = document.getElementById("generated-array"); i < generatedArray.length; i++) { //Divides array to separate divs
      const data = generatedArray[i];
-     const displayedDiv = `<div>${data}</div>`;
+     const displayedDiv = `<div class="specific-letter" onclick="click()">${data}</div>`;
      container.insertAdjacentHTML("beforeend", displayedDiv);
 }
 
-console.log(jsonData);
+let click = () => {
+     let letters = document.getElementsByClassName("specific-letter");
+     let result = document.getElementById("names");
+
+     for (let i = 0; i < letters.length; i++) {
+          letters[i].addEventListener("click", () => {
+               result.innerHTML = jsonData[1].name
+          }, false);
+     }
+}
+
+click();
+
